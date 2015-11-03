400495010b14b9812871fb9ebf6f345759370f70
wuxingzhong <330332812@qq.com>
2015-11-03 18:49:51 +0800

    add

---
+++ include/VideoData.h
@@ -0,0 +1,30 @@
+#include "stdafx.h"
+
+class  CVideoData
+{
+public:
+	CVideoData(void* buf = NULL);
+	~CVideoData(void);
+	/*送入数据*/
+	void OnData(void* buf);
+
+	/*获取视频数据*/
+	uchar_t* GetPdata() const;
+
+	/*打印信息*/
+	void print_info() const ;
+
+protected:
+
+private:
+	/*video 信息*/
+	uchar_t m_frame_type :4;			//frame类型
+	uchar_t m_codec_id   :4;			//编码器id    7 -- H264/AVC
+	
+	/*video 数据 H264/AVC*/
+	uchar_t m_AVC_packet_type;			//0:  AVC sequence header 1:  AVC NALU  2:  AVC end of sequence
+	uchar_t m_composition_time[3];   
+	uchar_t* m_pdata;					//AVC_packet_type == 0   AVC_packet_type == 1 One or more NALUs  Else Empty
+
+};
+
---
+++ src/VideoData.cpp
@@ -0,0 +1,55 @@
+#include "VideoData.h"
+#include "FLVTool.h"
+
+CVideoData::CVideoData( void* buf )
+{
+	m_pdata = NULL;
+	OnData(buf);
+}
+
+CVideoData::~CVideoData(void)
+{
+
+}
+
+/*送入数据*/
+void CVideoData::OnData(void* buf)
+{
+	//初始化为0 
+	memset(this, 0, sizeof(*this));
+	if(buf == NULL)
+	{
+		return ;
+	}
+	//转换数据
+	*this = *(CVideoData*)buf;
+	if(m_AVC_packet_type == 0 || m_AVC_packet_type == 1)
+	{
+		m_pdata = (uchar_t*)buf + 5;
+	}
+	else
+	{
+		m_pdata = NULL;
+	}
+}
+
+
+/*获取视频数据*/
+
+uchar_t* CVideoData::GetPdata() const 
+{ 
+	return m_pdata; 
+}
+
+/*打印信息*/
+void CVideoData::print_info() const 
+{
+
+	printf("m_frame_type         = %d\n", m_frame_type);
+	printf("m_codec_id           = %d\n", m_codec_id);
+	printf("m_AVC_packet_type    = %d\n", m_AVC_packet_type);
+	printf("m_composition_time   = %d\n", CFLVTool::rev_toi(m_composition_time, 3) );
+	printf("m_frame_type         = %d\n", m_frame_type);
+	printf("m_pdata              = %#x\n\n", m_frame_type);
+	    
+}
